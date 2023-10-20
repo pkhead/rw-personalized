24ba6b77b57ccd0f7cbf22669228a982c02f51d2
pkhead <chromosoze@gmail.com>
2023-10-20 14:53:41 -0500

    slightly improved config menu change config defaults

--- plugin/gaming/Config.cs
+++ plugin/gaming/Config.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Menu.Remix.MixedUI;
 using UnityEngine;
 
@@ -9,7 +10,7 @@ namespace RWMod
 
         public static Configurable<int> ShinyChance = instance.config.Bind(
             key: "shinyChance",
-            defaultValue: 10,
+            defaultValue: 20,
             info: new ConfigurableInfo(
                 "The chance that some creatures will be \"shiny\"",
                 new ConfigAcceptableRange<int>(0, 100),
@@ -20,7 +21,7 @@ namespace RWMod
 
         public static Configurable<int> SpawnChance = instance.config.Bind(
             key: "spawnChance",
-            defaultValue: 5,
+            defaultValue: 2,
             info: new ConfigurableInfo(
                 "The chance that IT will spawn",
                 new ConfigAcceptableRange<int>(0, 100),
@@ -35,14 +36,12 @@ namespace RWMod
         public override void Initialize()
         {
             base.Initialize();
-            tabIndex = -1;
-            curY = MENU_TOP - ITEM_HEIGHT;
-            curX = 5;
-            Tabs = new OpTab[1];
-            
+            InitBuilder(1);
+
             AddTab("General");
-            AddSlider("Shiny Chance", "The chance that some creatures will be \"shiny\"", ShinyChance, 160);
-            AddSlider("Spawn Chance", "The chance that IT will spawn", SpawnChance, 160);
+            Title("Personalizer");
+            AddSlider("Shiny Chance", "The chance that some creatures will be differently colored", ShinyChance, 160);
+            AddSlider("Spawn Chance", "The chance that it will spawn", SpawnChance, 160);
         }
 
         #region UI Builder functions
@@ -50,20 +49,41 @@ namespace RWMod
         private const float ITEM_HEIGHT = 30f;
         private const float ITEM_MARGIN_Y = 5f;
         private const float LABEL_MARGIN_X = 20f;
-        private const float MENU_TOP = 500f;
+        private const float MENU_TOP = 600f - 50f;
+        private const float MENU_RIGHT = 600f; // i don't really know
 
         private int tabIndex = -1;
         private float curY = MENU_TOP - ITEM_HEIGHT;
         private float curX = 5;
+
+        private void InitBuilder(int tabCount)
+        {
+            tabIndex = -1;
+            curY = MENU_TOP - 60;
+            curX = 0;
+            Tabs = new OpTab[tabCount];
+        }
 
         private void AddTab(string tabName)
         {
             Tabs[++tabIndex] = new OpTab(this, tabName);
         }
 
+        private void Title(string text)
+        {
+            var label = new OpLabel(
+                new Vector2(0, MENU_TOP - 10f),
+                new Vector2(MENU_RIGHT, 10f), text, FLabelAlignment.Center, true);
+            
+            Tabs[tabIndex].AddItems(label);
+        }
+
         private void AddSlider(string text, string desc, Configurable<int> config, int width)
         {
-            var slider = new OpSlider(config, new Vector2(curX, curY), width);
+            var slider = new OpSlider(config, new Vector2(curX, curY), width) {
+                description = desc
+            };
+
             var label = new OpLabel(curX + slider.size.x + LABEL_MARGIN_X, curY + (slider.size.y - LabelTest.LineHeight(false)) / 2, text, false);
             Tabs[tabIndex].AddItems(slider, label);
             curY -= slider.size.y + ITEM_MARGIN_Y;
